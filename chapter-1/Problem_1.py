72ed9d2f830af328ddd5c4dfe73a0fddd7a7997d
Ramaruva <ramaruva54@gmail.com>
2025-08-28 16:42:03 -0500

    basic inital py learning

---
+++ chapter-1/Problem_1.py
@@ -0,0 +1,13 @@
+print('''
+I’m a liar. I’m not okay, not good— I’m tired, and sometimes I just want to scream.
+
+I accept my fate as the mask slowly cracks. I lie, and it patches itself back together, stitched shut by the threads of my fractured tales.
+
+They say if the truth hurts, then lies must be kind. Yeah, right. They’re just lies— simple shields to hide the pain from others, to keep them blind to the madness in me, screaming quietly, waiting to explode into horror.
+
+If you ever catch me lying, know it’s for your own good. Inside me is something that might make you want to lie too.
+
+Don’t be infected by me. Even as a liar, I hate liars. I’m a hypocrite— because I know what they hide.
+
+They hide the truth. They hide the pain. They hide themselves.
+''')
