3023e11a7b8ccfb50a52558fd82ec7df95a00662
Ramaruva <ramaruva54@gmail.com>
2025-09-05 14:39:51 -0500

    strings

---
+++ chapter-3/01_intro_to_strings.py
@@ -0,0 +1,31 @@
+name ="ramakkk"
+name2=name[0:1]
+print(name2)
+
+char1=name[0]
+print(char1)
+charlast = name[-1]
+print(charlast)
+
+negname = "Harry"
+
+print(negname[-4:-1])
+
+print(len(name))
+print(name.endswith("jaja"))
+print(name.startswith("Ra"))
+print(name.capitalize())
+
+
+#practice set
+username = input("Enter your name:")
+print(f"Good Afternoon {username}")
+
+letter='''
+   Dear <|Name|>,
+   You are selected!
+   <|Date|>
+'''
+
+print(letter.replace("<|Name|>","Ram").replace("<|Date|>","5sept"))
+
