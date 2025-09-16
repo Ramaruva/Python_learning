beac31f1cff56ecfe165ef2217267870cd342639
Ramaruva <ramaruva54@gmail.com>
2025-09-16 16:09:42 -0500

    dict and sets

---
+++ chapter-5/01_dict.py
@@ -0,0 +1,15 @@
+marks={
+    "haryy":100,
+    "shubam":56,
+    "rohan":23
+}
+print(marks)
+print(type(marks))
+print(marks["haryy"])
+
+# methods
+print(marks.items())
+print(marks.keys())
+marks.update({"haryy":98})
+print(marks)
+print(marks.get("rohan"))
---
+++ chapter-5/02_sets.py
@@ -0,0 +1,8 @@
+s = {1,23,44}
+empty =  set()
+
+print(empty,s)
+
+s.add(3)
+print(s)
+print(len(s))
