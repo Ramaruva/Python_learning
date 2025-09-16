be6d6b9dd6c1ca8846b6d171219caa80cc85092a
Ramaruva <ramaruva54@gmail.com>
2025-09-16 15:08:28 -0500

    lists and tuple

---
+++ chapter-4/01_list.py
@@ -0,0 +1,25 @@
+friends =["Apple","Orange",5,345.6,False]
+print(friends[0])
+friends[1]="jaja"
+print(friends[1:4])
+
+
+# Methods
+friends.append("ram")
+print(friends)
+
+l1=[1,23,5,6,7,3,33,67]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+
+l1.insert(3,333)
+
+print(l1)
+l1.pop(2)
+l1.remove(1)
+print(l1)
+
+
+
---
+++ chapter-4/02_tuple.py
@@ -0,0 +1,6 @@
+a =(1,2,3,45,5)
+print(a)
+
+# Methods
+no = a.count(45)
+print(no)
