a46b9d19577204d3f9139982427e10b146f2b981
Ramaruva <ramaruva54@gmail.com>
2025-09-16 16:37:36 -0500

    conditions

---
+++ chapter-6/01_conditional.py
@@ -0,0 +1,10 @@
+a = int(input("enter your age:"))
+
+if a>=18:
+    print("Your above the age of consent")
+elif a<0:
+    print("Your entering invalid age")
+else:
+    print("Your below the age consent")
+
+print("End of the program")
