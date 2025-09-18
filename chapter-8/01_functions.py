04c2b908bc187944aadd17ec62006eefdc37dc11
Ramaruva <ramaruva54@gmail.com>
2025-09-17 20:12:28 -0500

    func

---
+++ chapter-8/01_functions.py
@@ -0,0 +1,25 @@
+def avg():
+    a = int(input("Enter your number:"))
+    b = int(input("Enter your number:"))
+    c = int(input("Enter your number:"))
+    average = (a+b+c)/3
+    print(average)
+
+avg()
+
+def goodday():
+    print("Good day!")
+
+goodday()
+
+
+# func args
+
+def goodday(name="heyy"):
+    print("Good day"+name)
+
+goodday()
+
+
+
+
