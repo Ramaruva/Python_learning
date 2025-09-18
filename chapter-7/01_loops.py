a5ce693c1b6fae8b1bfa58ebdbb42242f97ab90a
Ramaruva <ramaruva54@gmail.com>
2025-09-17 19:51:50 -0500

    loops

---
+++ chapter-7/01_loops.py
@@ -0,0 +1,45 @@
+for i in range(1,6):
+    print(i)
+
+
+# while loop
+i=1
+while i<=6:
+    print(i)
+    i+=1
+
+list =[1,"hraar",2,"3","aaa",4,5,6]
+
+i=0
+while i<len(list):
+    print(list[i])
+    i+=1
+
+for i in range(0,len(list)):
+    print(list[i])
+
+
+for i in list:
+    print(i)
+
+t=(2,34,5,6,7)
+
+for i in t:
+    print(i)
+
+
+# break , continue
+
+for i in range(100):
+    print(i)
+    if i==35:
+        break
+
+for i in range(100):
+    if i == 35:
+        continue
+    print(i)
+
+
+
+
